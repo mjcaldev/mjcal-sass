6a3ecea6afefdfa1a502fae11f69bd379e041e9c
Micheal Callaghan <mehaul96@gmail.com>
2025-09-23 01:18:21 -0400

    navbar made into client component and mapping over navitems successfully

---
+++ components/NavItems.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const navItems = [
+  { label: 'Home', href: '/' },
+  { label: 'Companions', href: '/companions' },
+  { label: 'My Journey', href: 'my-journey' }
+]
+
+const NavItems = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link href={href} key={label}>
+          {label}
+        </Link>
+      ))}
+    </nav>
+  )
+}
+
+export default NavItems
