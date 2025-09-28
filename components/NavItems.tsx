d7fb8179db74fe43f697ff0c1607eb867a9512de
Micheal Callaghan <mehaul96@gmail.com>
2025-09-28 00:14:18 -0400

    completed lesson cards

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -7,7 +7,8 @@ import { usePathname } from "next/navigation"
 const navItems = [
   { label: 'Home', href: '/' },
   { label: 'Companions', href: '/companions' },
-  { label: 'My Journey', href: 'my-journey' }
+  { label: 'My Journey', href: 'my-journey' },
+  { label: 'Sign In', href: '/signin'}
 ]
 
 const NavItems = () => {
