617290f422ff50fab489c68a8ce9da8568a82318
Micheal Callaghan <mehaul96@gmail.com>
2025-09-29 00:36:43 -0400

    started recent sessions tables

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -1,7 +1,45 @@
-const CompanionsList = () => {
-  return (
-    <div>CompanionsList</div>
-  )
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface CompanionsListProps {
+  title: string;
+  companions?: Companion[];
+  classNames?: string;
 }
 
-export default CompanionsList
+const CompanionsList = ({ title, companions, classNames }: CompanionsListProps) => {
+  return (
+    <article>
+      <h2>Recent Sessions</h2>
+
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </article>
+  );
+};
+
+export default CompanionsList;
